c458190023edf482281570a3f58aef77497fadc2
manager-api <tedyfiromi@gmail.com>
2020-07-25 16:03:03 -0300

    Initial Commit

---
+++ src/main/java/br/com/itau/ititecnoapi/ItiTecnoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.ititecnoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItiTecnoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItiTecnoApiApplication.class, args);
+    }
+
+}
